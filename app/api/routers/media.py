1bd456b1a4c1aba6b1c4de185163c1e4fa2ed91b
Charles <charles@charlesen.fr>
2025-03-01 11:32:36 +0100

    Text to speech wutg gtts

---
+++ app/api/routers/media.py
@@ -0,0 +1,59 @@
+import uuid
+import os
+from typing import Optional
+import pyttsx3
+from gtts import gTTS
+
+from fastapi import APIRouter, HTTPException, Depends, Request
+from pydantic import BaseModel
+
+from app.dependencies import verify_master_key
+router = APIRouter(dependencies=[Depends(verify_master_key)])
+
+class AudioResponse(BaseModel):
+    audio_url: str
+
+@router.post("/text-to-speech", response_model=AudioResponse, tags=["Media"])
+async def text_to_speech(text: str, request: Request, language: Optional[str] = "fr"):
+    try:
+        # Initialisation du moteur gtts        
+        tts = gTTS(text, lang=language)
+        # Chemin du fichier audio avec le UUID
+        # UUID unique pour le nom du fichier
+        unique_id = uuid.uuid4()
+        audio_filename = f"{unique_id}.mp3"
+        audio_path = os.path.join("public", "medias", audio_filename)
+        
+        tts.save(audio_path)
+
+        # URL absolue du fichier audio
+        audio_url = f"{request.base_url}/public/medias/{audio_filename}"
+
+        return AudioResponse(audio_url=audio_url)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erreur lors de la conversion du texte en parole : {str(e)}")
+    
+
+@router.post("/text-to-speech-low", response_model=AudioResponse, tags=["Media"])
+async def text_to_speech_low(text: str, request: Request, language: Optional[str] = "fr"):
+    try:
+        # UUID unique pour le nom du fichier
+        unique_id = uuid.uuid4()
+        # Chemin du fichier audio avec le UUID
+        audio_filename = f"{unique_id}.mp3"
+        audio_path = os.path.join("public", "medias", audio_filename)
+
+        # Initialisation du moteur pyttsx3
+        engine = pyttsx3.init()
+        engine.setProperty('voice', language)  # Sélection de la langue
+        engine.save_to_file(text, audio_path)
+        engine.runAndWait()
+
+        # URL absolue du fichier audio
+        # base_url = os.getenv("BASE_URL", "http://127.0.0.1:8000")  # Valeur par défaut si la variable n'est pas définie
+        # audio_url = f"{base_url}/public/medias/{audio_filename}"
+        audio_url = f"{request.base_url}/public/medias/{audio_filename}"
+
+        return AudioResponse(audio_url=audio_url)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erreur lors de la conversion du texte en parole : {str(e)}")
--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
+# System
+import os
+
 # FastAPI
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
 
 # Routers
 from app.api.routers.openai import router as openai_router
+from app.api.routers.media import router as media_router
 from app.config import settings
 
 
 app = FastAPI(title="AI API by Charles EDOU NZE", debug=settings.debug)
 
+# On monte le répertoire 'public' pour servir des fichiers statiques
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# public_dir = os.path.join(current_dir, 'public')
+# app.mount("public", StaticFiles(directory=public_dir), name="public")
+app.mount("/public", StaticFiles(directory="public"), name="public")
+
 # Endpoints de l'API publique
 app.include_router(openai_router, prefix="/api/openai", tags=["Generate"])
+app.include_router(media_router, prefix="/api/media", tags=["Media"])
 
 # Redirige la racine vers l'admin
 @app.get("/", include_in_schema=False)
